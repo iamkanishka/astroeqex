11478f521f4743b20478467fee5ecfb39b4ce190
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-17 21:45:08 +0530

    Added Deps

--- mix.exs
+++ mix.exs
@@ -1,13 +1,31 @@
 defmodule AstroEquations.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/iamkanishka/appwrite"
+  @version "0.1.0"
+
   def project do
     [
       app: :astro_equations,
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description:
+        " A collection of core equations and constants from astronomy, astrophysics, and cosmology .",
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      name: "astro_equations",
+      # License, e.g., MIT, Apache 2.0
+      licenses: ["Apache-2.0"],
+      links: %{
+        GitHub: @source_url
+      },
+      maintainers: ["Kanishka Naik"]
     ]
   end
 
@@ -23,6 +41,8 @@ defmodule AstroEquations.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 end
