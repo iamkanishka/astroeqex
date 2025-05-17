f01c24ea31d5975d979cf3b1ee8cdf457234460b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-17 21:42:48 +0530

    Initial commit

---
+++ lib/astro_equations.ex
@@ -0,0 +1,18 @@
+defmodule AstroEquations do
+  @moduledoc """
+  Documentation for `AstroEquations`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AstroEquations.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/astro_equations_test.exs
@@ -0,0 +1,8 @@
+defmodule AstroEquationsTest do
+  use ExUnit.Case
+  doctest AstroEquations
+
+  test "greets the world" do
+    assert AstroEquations.hello() == :world
+  end
+end
