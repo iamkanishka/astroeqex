a931b89cda808a1d88edf2a06775f2db39cb2b5a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-29 18:00:57 +0530

    Added deps

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule AstroEquations.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:ex_doc, "~> 0.28", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
+      {:mox, "~> 1.0", only: :test}
     ]
   end
 end
