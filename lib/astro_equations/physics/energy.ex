5ddf68c1449387dd790946d208fa4f6da127406b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-06-04 14:27:11 +0530

    chore: Updated comments

--- lib/astro_equations/physics/energy.ex
+++ lib/astro_equations/physics/energy.ex
@@ -2,8 +2,7 @@ defmodule AstroEquations.Physics.Energy do
   @moduledoc """
   A collection of fundamental physics formulas and calculations.
 
-  This module provides functions for calculating work, buoyancy, friction,
-  wave properties, and related physics concepts.
+  This module provides functions for calculating work  related physics concepts.
   """
 
   @doc """
@@ -14,10 +13,10 @@ defmodule AstroEquations.Physics.Energy do
     - displacement_vector: The displacement vector [dx, dy, dz]
 
   ## Examples
-      iex> Physics.work([2, 0, 0], [3, 0, 0])
+      iex> AstroEquations.Physics.Energy.work([2, 0, 0], [3, 0, 0])
       6.0
 
-      iex> Physics.work([1, 2, 3], [4, 5, 6])
+      iex> AstroEquations.Physics.Energy.work([1, 2, 3], [4, 5, 6])
       32.0
   """
   @spec work(list(number), list(number)) :: float
